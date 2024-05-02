48d882cb48000f4783a148a0e24b1698e35cc7dc
ricardotimmr <ricardo.timmr@gmail.com>
2024-05-02 10:36:38 +0200

    Added the Tab-Navigation in JS CSS and HTML

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,34 @@ button.addEventListener("click", () => {
     menu.classList.toggle("is-active");
     button.classList.toggle("is-active");
 });
+
+    const tabLinks = document.querySelectorAll(".tab-link");
+    const tabContents = document.querySelectorAll(".tab-content");
+    const mainContent = document.querySelector("main");
+
+    tabLinks.forEach(tabLink => {
+        tabLink.addEventListener("click", function() {
+            const tabId = this.getAttribute("data-tab");
+
+            const isMeinBereich = this.textContent.trim() === "Mein Bereich";
+
+            if (!isMeinBereich) {
+                mainContent.classList.add("is-hidden");
+            } else {
+                mainContent.classList.remove("is-hidden");
+            }
+
+            tabLinks.forEach(link => {
+                link.classList.remove("is-active");
+            });
+
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove("is-active");
+            });
+
+            this.classList.add("is-active");
+
+            const selectedTabContent = document.getElementById(tabId);
+            selectedTabContent.classList.add("is-active");
+        });
+    });
