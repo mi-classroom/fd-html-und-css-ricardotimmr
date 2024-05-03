e8c3be86a9a8a09d6743474c93b05565a3bf7413
ricardotimmr <ricardo.timmr@gmail.com>
2024-05-03 15:02:58 +0200

    created schlagwörter list dinamcally

--- scripts/articlelist.js
+++ scripts/articlelist.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+// Fetch the article list with the filtered buttons
+
 function createArticleList() {
   const responsePromise = fetch(articleListURL);
   responsePromise.then(function (response) {
@@ -22,6 +24,8 @@ function createArticleList() {
 
 
 
+// Filter the articles by keyword
+
 function filterArticles(filterValue){
   const filteredArticles = currentData.articles.filter(function(article){
     return article.tags.keywords.includes(filterValue);
@@ -30,7 +34,10 @@ function filterArticles(filterValue){
 }
 
 
-function initFilters(){
+
+// Initialize the buttons as filters
+
+/* function initFilters(){
   const filterButtons = document.querySelectorAll('[data-js-category="keywords"] [data-js-filter');
   filterButtons.forEach(function(button){
     button.addEventListener('click', function(event){
@@ -39,8 +46,10 @@ function initFilters(){
       renderArticleList(filteredArticles);
     });
   });
-}
+} */
 
+
+// Render the article list
 
 function renderArticleList(articles) {
   const articleListElement = document.querySelector('[data-js-generate-articleList]')
@@ -67,4 +76,34 @@ function renderArticleList(articles) {
 
     articleListElement.innerHTML = cards;
     
+}
+
+
+function initFilters(){
+  const filterSection = document.querySelector('[data-js-category="keywords"]');
+  const allTags = currentData.articles.reduce((list, article) => {
+    article.tags.keywords.forEach(tag => {
+      if (!list.includes(tag)) {
+        list.push(tag);
+      }
+    });
+    return list;
+  }, []);
+
+  const filterButtonsHTML = allTags.map(tag => `<li><button class="button button-primary" data-js-filter="${tag}">${tag}</button></li>`).join('');
+  
+  filterSection.innerHTML = filterButtonsHTML;
+
+
+
+  // Event listeners for the generated buttons
+
+  const filterButtons = filterSection.querySelectorAll('button');
+  filterButtons.forEach(function(button){
+    button.addEventListener('click', function(event){
+      const filter = event.currentTarget.getAttribute('data-js-filter');
+      const filteredArticles = filterArticles(filter);
+      renderArticleList(filteredArticles);
+    });
+  });
 }
